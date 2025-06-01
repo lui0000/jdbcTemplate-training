fa979b0311aecd6b1a328753522fe5ba26c194a6
Eliza <elbondarenko04@gmail.com>
2025-06-01 22:10:02 +0300

    first commit

---
+++ src/test/java/com/eliza/jdbcTemplate/JdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliza.jdbcTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
